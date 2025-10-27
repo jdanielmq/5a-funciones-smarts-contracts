aaab781dc8ddc3d6e75db4239e79c28d7885a477
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2025-10-27 15:17:09 -0300

    se agrega la nueva funcionalidad o ejercicios donde gurdadmos un regisrtos comvertido como hash

--- contracts/5a.funciones.sol
+++ contracts/5a.funciones.sol
@@ -16,4 +16,49 @@ contract Testfunctions {
     function obtenerLongitud() public view returns(uint) {
         return direcciones.length;
     }
+
+    //functions two
+    bytes32 public hash;
+    function calcularHash(string memory _cadena) public {
+        hash = keccak256(abi.encodePacked(_cadena));
+    }
+
+    //fucntion three: hacer uso de un tipo complejo llamado "comida" con nombre. precio e ingredientes
+
+    struct Comida {
+        string nombre;
+        uint precio;
+        string ingredientes;
+    }
+
+    Comida public paella;
+
+    function prepararPaella(string memory _nombre, uint _precio, string memory _ingredientes) public {
+        paella = Comida(_nombre, _precio, _ingredientes);
+
+    }
+
+    //function flour: trabajar con funciones privadas
+
+    struct Futbolista {
+        string nombre;
+        uint dorsal;
+        address direccion;
+    }
+
+    bytes32 public hashFutbolista;
+
+    //calcular el hash de un futbolista
+    function cacularHashFutbolista(string memory _nombre, uint _dorsal, address) private {
+        hashFutbolista = keccak256(abi.encodePacked(_nombre, _dorsal, msg.sender));
+    }
+
+    Futbolista[] public futbolistas;
+    mapping (string => bytes32) public futbolistasGuardados;
+    function agregarFutbolista(string memory _nombre, uint _dorsal) public {
+        cacularHashFutbolista(_nombre, _dorsal, msg.sender);
+        futbolistas.push(Futbolista(_nombre, _dorsal, msg.sender));
+        futbolistasGuardados[_nombre] = hashFutbolista;
+    }
+
 }
